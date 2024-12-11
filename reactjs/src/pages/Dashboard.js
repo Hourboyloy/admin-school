c0b5261f623b66112905a2fbcabba4c21252443f
Hourboyloy <chhoysienghour@gmail.com>
2024-12-11 16:16:26 +0700

    Updated

--- reactjs/src/pages/Dashboard.js
+++ reactjs/src/pages/Dashboard.js
@@ -4,7 +4,6 @@ import { CreateContext } from "../Helper/Context";
 
 const Dashboard = () => {
   const { dataTotalViewsAnalysis } = useContext(CreateContext);
-  
   return (
     <div className="container mx-auto md:p-6 p-4">
       <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-6 mb-8">
@@ -13,8 +12,10 @@ const Dashboard = () => {
           <p className="lg:text-2xl md:text-xl text-lg font-bold">10</p>
         </div>
         <div className="bg-green-100 p-4 rounded-md text-center">
-          <h3 className="md:text-lg font-medium">Total Views</h3>
-          <p className="lg:text-2xl md:text-xl text-lg font-bold">500</p>
+          <h3 className="md:text-lg font-medium">Total Views in 12 Months</h3>
+          <p className="lg:text-2xl md:text-xl text-lg font-bold">
+            {dataTotalViewsAnalysis?.totalViewsLast12Months ?dataTotalViewsAnalysis?.totalViewsLast12Months:"N/A"}
+          </p>
         </div>
       </div>
 
